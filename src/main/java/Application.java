6fc9ce280e78d523a4549667e108f986e4ec6af6
woniper <leekw3747@gmail.com>
2014-10-25 14:06:34 +0900

    설정 및 Controller 작성

---
+++ src/main/java/Application.java
@@ -0,0 +1,17 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by woniper on 14. 10. 25..
+ */
+
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
