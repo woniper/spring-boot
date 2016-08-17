932da0419310a4461d17d9be7e45fcce25c57a3e
KyungWonLee <lkw1989@wemakeprice.com>
2016-08-17 11:42:57 +0900

    changed source and version

--- src/main/java/net/woniper/springboot/Application.java
+++ src/main/java/net/woniper/springboot/Application.java
@@ -1,15 +1,13 @@
 package net.woniper.springboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by woniper on 14. 10. 25..
  */
 
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
